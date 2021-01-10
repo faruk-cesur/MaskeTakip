55ecdccf5ae36f92883cc258d20ebb24784eea0f
faruk-cesur <faruk-cesur@hotmail.com>
2021-01-10 18:04:57 +0300

    10.01.2020

---
+++ Business/Concrete/ForeignerManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Business.Abstract;
+using Entities.Concrete;
+
+namespace Business.Concrete
+{
+    public class ForeignerManager : IApplicantService
+    {
+        public void ApplyForMask(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CheckPerson(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Person> GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
